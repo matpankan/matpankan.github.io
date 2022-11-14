3e622db57c84f668b35fa0174b8d941a9d5a746b
matpankan <116716977+matpankan@users.noreply.github.com>
2022-11-14 10:31:27 +0100

    Add files via upload

---
+++ akinator/index.js
@@ -0,0 +1,7 @@
+import { Button } from 'elemental'
+import {a} from "./olvver.js";
+function startbutton()  {
+    console.log('jajajajaj<s')
+}
+
+// document.getElementById('test').innerHTML=('xc')
