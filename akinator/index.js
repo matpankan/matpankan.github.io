ce153d38e057c4732e2689739f8368efec53886f
matpankan <116716977+matpankan@users.noreply.github.com>
2022-11-14 10:44:34 +0100

    Delete akinator directory

--- akinator/index.js
+++
@@ -1,7 +0,0 @@
-import { Button } from 'elemental'
-import {a} from "./olvver.js";
-document.getElementById("button").addEventListener("click", () => {
-    console.log("Hello!");
-});
-
-// document.getElementById('test').innerHTML=('xc')
